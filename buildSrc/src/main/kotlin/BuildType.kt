23c5e1a1228b5289b686298842ce2f50340fcd0a
aptx113 <aptx113@gmail.com>
2021-03-08 23:39:24 +0800

    create AndroidConfig, BuildFeatures and BuildType file

---
+++ buildSrc/src/main/kotlin/BuildType.kt
@@ -0,0 +1,26 @@
+/**
+ * Created by George Yu on 2021/3/8.
+ */
+interface BuildType {
+
+    companion object {
+        const val DEBUG = "debug"
+        const val RELEASE = "release"
+    }
+
+    val isMinifyEnabled: Boolean
+    val isTestCoverageEnabled: Boolean
+}
+
+object BuildTypeDebug : BuildType {
+    override val isMinifyEnabled: Boolean = false
+    override val isTestCoverageEnabled: Boolean = true
+
+    const val APPLICATION_ID_SUFFIX = ".debug"
+    const val VERSION_NAME_SUFFIX = "-DEBUG"
+}
+
+object BuildTypeRelease : BuildType {
+    override val isMinifyEnabled: Boolean = true
+    override val isTestCoverageEnabled: Boolean = false
+}
