6543dbb38e47a3b029ad1ee8907b0febb3470db5
aptx113 <aptx113@gmail.com>
2021-12-05 14:44:59 +0800

    modify gradle

--- buildSrc/src/main/kotlin/PluginVersions.kt
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -1,5 +1,5 @@
 object PluginVersions {
-    const val DETEKT_VERSION = "1.17.1"
+    const val DETEKT_VERSION = "1.19.0"
     const val DOKKA_VERSION = "1.4.32"
     const val GRADLE_VER_PLUGIN_VERSION = "0.39.0"
     const val KTLINT_VERSION = "10.1.0"
--- buildSrc/src/main/kotlin/dependencyVersions/TestDependencyVersions.kt
+++ buildSrc/src/main/kotlin/dependencyVersions/TestDependencyVersions.kt
@@ -4,7 +4,7 @@ package dependencyVersions
 object TestDependencyVersions {
     const val ARCH_VERSION = "2.1.0"
     const val JUNIT_VERSION = "4.13.2"
-    const val MOCKK_VERSION = "1.12.0"
-    const val ROBOLECTRIC_VERSION = "4.6.1"
+    const val MOCKK_VERSION = "1.12.1"
+    const val ROBOLECTRIC_VERSION = "4.7.3"
     const val TRUTH_VERSION = "1.1.3"
 }
